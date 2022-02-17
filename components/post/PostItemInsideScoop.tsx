35ee57b179be222d5ae490086f659297f7d6cdc2
Junyu Yang <prowessyang@gmail.com>
2022-02-17 00:41:56 -0700

    order insight by total eth spent, fixed date display

--- components/post/PostItemInsideScoop.tsx
+++ components/post/PostItemInsideScoop.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Box, Button, Heading, Stack, Table, Thead, Tbody, Tr, Th, Td, TableCaption } from "@chakra-ui/react";
 import { FaRocket } from "react-icons/fa";
-import { compareDesc, parse } from "date-fns";
+import { format, parse } from "date-fns";
 type Props = {
   post: any;
 };
 const PostItemInsideScoop: React.FC<Props> = ({ post }) => {
   const [showTable, setShowTable] = React.useState(false);
-  const orderedInsights = [...post.collection.insight];
-  orderedInsights.sort((a, b) => a.total_eth_spent > b.total_eth_spent ? -1 : -1)
+  const orderedInsights = [...post.collection.insight].sort((a, b) => a.total_eth_spent > b.total_eth_spent ? -1 : 1);
   return (
     <Box mt={5}>
       <Stack
@@ -25,7 +24,7 @@ const PostItemInsideScoop: React.FC<Props> = ({ post }) => {
         <Thead>
           <Tr>
             <Th>Address</Th>
-            <Th>Eth</Th>
+            <Th>Total Eth Spent</Th>
             <Th>When</Th>
           </Tr>
         </Thead>
@@ -36,7 +35,7 @@ const PostItemInsideScoop: React.FC<Props> = ({ post }) => {
               <Tr key={insider_id}>
                 <Td>{insider_id}</Td>
                 <Td>{Math.round(total_eth_spent * 100) / 100}</Td>
-                <Td>{started_at}</Td>
+                <Td>{format(parse(started_at.substring(0, 10), "yyyy-MM-dd", new Date()), "MMM-d-yyyy")}</Td>
               </Tr>
             ))}
         </Tbody>
