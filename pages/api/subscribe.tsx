6ef7b5274a6fb364b6d097d122a496ecc6abc898
Junyu Yang <prowessyang@gmail.com>
2022-02-27 10:06:22 -0800

    have api route /api/subscribe

---
+++ pages/api/subscribe.tsx
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+
+    // if (req.method !== 'POST') {
+    //     res.status(400).send({ message: 'Only POST requests allowed' })
+    //     return
+    // }
+
+    // const mailchimp = require('@mailchimp/mailchimp_marketing');
+
+    // mailchimp.setConfig({
+    //     apiKey: 'YOUR_API_KEY',
+    //     server: 'us14'
+    // });
+
+    // const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY
+    // res.status(200).json({ name: MAILCHIMP_API_KEY })
+    res.status(200).json({ name: "MAILCHIMP_API_KEY" })
+}
