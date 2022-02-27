28a7fc7628ab88f3c94a275601d6e0c024bb9ea1
Junyu Yang <prowessyang@gmail.com>
2022-02-27 13:35:43 -0800

    base

--- pages/api/subscribe.tsx
+++ pages/api/subscribe.tsx
@@ -1,18 +1,36 @@
-export default function handler(req, res) {
+// const mailchimp = require('@mailchimp/mailchimp_marketing');
+const mailchimpFactory = require("@mailchimp/mailchimp_transactional/src/index.js")
+const mailchimp = mailchimpFactory(process.env.MANDRILL_API_KEY)
+
+export default async function handler(req, res) {
 
     if (req.method !== 'POST') {
         res.status(400).send({ message: 'Only POST requests allowed' })
         return
     }
 
-    // const mailchimp = require('@mailchimp/mailchimp_marketing');
+    if (!req.body.email) {
+        res.status(400).send({ message: 'request body should contain email field' })
+        return
+    }
 
+    const email = req.body.email
+
+    console.log("🐵")
+
+
+    const response = await mailchimp.users.ping2();
+    console.log(response);
     // mailchimp.setConfig({
-    //     apiKey: 'YOUR_API_KEY',
+    //     apiKey: process.env.MAILCHIMP_API_KEY,
     //     server: 'us14'
     // });
 
-    // const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY
-    // res.status(200).json({ name: MAILCHIMP_API_KEY })
-    res.status(200).json({ name: "MAILCHIMP_API_KEY" })
+    // const response = await fetch(process.env.MAILCHIMP_API_ENDPOINT + '/users/ping')
+    // console.log(response)
+
+    res.status(200).json({ email: email })
 }
+
+
+
