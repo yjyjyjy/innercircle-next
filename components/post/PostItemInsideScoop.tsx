07b787a22dac68aa3ab2bdf4e0f630a159cbe6fb
Junyu Yang <prowessyang@gmail.com>
2022-03-22 16:21:07 -0700

    chart with tooltip. profile pic

--- components/post/PostItemInsideScoop.tsx
+++ components/post/PostItemInsideScoop.tsx
@@ -18,7 +18,7 @@ const PostItemInsideScoop: React.FC<Props> = ({ post }) => {
   ); // desc rank by feed_importance_score. Bigger feed_importance_score rank higher
   const action_dict = { "buy": "bought", "sell": "sold" }
   return (
-    <Box pt={5} maxW='100%' overflow='scroll'>
+    <Box py={5} maxW='100%' overflow='scroll'>
       <Stack
         direction="row"
         justifyContent="center"
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,5 +1,4 @@
-import { Heading, Text, Button, Stack, Box } from "@chakra-ui/react";
-import Link from "next/link";
+import { Link, Flex, Image, Text, Button, Stack, Box, Heading } from "@chakra-ui/react";
 import prisma from "../../lib/prisma";
 import {
   CartesianGrid,
@@ -9,11 +8,13 @@ import {
   XAxis,
   YAxis,
   ZAxis,
-  Label
+  Label,
+  Tooltip
 } from "recharts";
 import { add, differenceInCalendarDays, format, sub } from "date-fns";
-import { useEffect, useRef, useState } from "react";
-import { GiConsoleController } from "react-icons/gi";
+// import { useEffect, useRef, useState } from "react";
+// import { GiConsoleController } from "react-icons/gi";
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 
 // server side data fetch
@@ -21,7 +22,14 @@ export async function getServerSideProps(context) {
   const { id } = context.query;
   const insider = await prisma.insider.findUnique({
     where: { id: id as string },
-    include: { insider_past_90_days_trading_roi: true },
+    include: {
+      insider_past_90_days_trading_roi: {
+        include: {
+          collection: true
+        }
+      },
+      insider_metadata: true
+    },
   });
 
   return {
@@ -52,32 +60,88 @@ const dateFormatter = (timestamp: number) => {
   return dateStr;
 };
 
-const User = ({ insider }) => {
+// const CustomTooltip = (payload) => {
+//   console.log(payload)
+//   return (
+//     <div >
+//       <p>FRESH</p>
+//     </div>
+//   );
 
-  const { id, insider_past_90_days_trading_roi } = insider
+// }
+
+const CustomTooltip = ({ active, payload }: any) => {
+  if (active && payload && payload.length) {
+    return (
+      <Box bg={'white'}>
+        <Text>Collection: {payload[0].payload.collection_name}</Text>
+        <Text>Investment: {Math.round(payload[0].payload.investment * 10) / 10} ETH</Text>
+        <Text>Return: {Math.round(payload[0].payload.roi * 10) / 10} %</Text>
+        <Text>Gain: {Math.round((payload[0].payload.investment * payload[0].payload.roi / 10) / 10)} ETH</Text>
+      </Box>
+    );
+  }
+
+  return null;
+};
+
+
+const User = ({ insider }) => {
+  const { id, insider_past_90_days_trading_roi, insider_metadata } = insider
   // const ref = useRef();
+  console.log(insider_past_90_days_trading_roi)
 
   const trades = insider_past_90_days_trading_roi.map(roi => ({
     "timestamp": new Date(roi.buy_date).getTime(),
     "roi": roi.roi_pct * 100,
     'investment': roi.buy_eth_amount,
-    'total_gain': Math.round(roi.total_gain)
+    'total_gain': Math.round(roi.total_gain),
+    'collection_name': roi.collection.name
   }))
 
   const maxDate = Math.max(...trades.map(t => t.timestamp)) + 86400000 * 8;
   const minDate = Math.min(...trades.map(t => t.timestamp)) - 86400000 * 8;
 
-
   return (
-    <Stack direction={'column'}>
-      <Text fontSize={18} fontWeight={'bold'} pt={10}>
-        Wallet Address:
-      </Text>
-      <Text fontSize={15}>{id}</Text>
+    <Stack direction={'column'} maxW={'100%'} overflow='scroll'>
+      <Flex h='120px' w='100%' direction={'row'} p={5}>
+        <Box
+          borderRadius={"50%"}
+          overflow="hidden"
+          border="2px"
+          borderColor={"white"}
+          width={100}
+          height={100}
+        >
+          <Image
+            src={insider_metadata.opensea_image_url}
+            objectFit={'cover'}
+            width={100}
+            height={100}
+          />
+        </Box>
+        <Flex ml={3} direction='column'>
+          <Heading as={'h2'}>
+            {insider_metadata.opensea_display_name}
+          </Heading>
+          <Text fontSize={15} pt={2}>
+            Address: <Link
+              href={`https://opensea.io/${id}`}
+              isExternal
+            >{`${id.substring(0, 3)}...${id.substring(id.length - 3, id.length)}`} <ExternalLinkIcon mx={[0, 2]} /></Link>
+          </Text>
+        </Flex>
+      </Flex>
+
+
+
       <Text fontSize={18} fontWeight={'bold'} pt={2}>Last 90 days Trading Gain: {trades[0].total_gain} ETH</Text>
       <Box>
-        <ScatterChart width={730} height={250}
-          margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+        <ScatterChart
+          width={600}
+          height={250}
+          margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+        >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis
             dataKey="timestamp"
@@ -93,7 +157,10 @@ const User = ({ insider }) => {
           >
             <Label value="Trade Return" angle={-90} offset={5} position="left" />
           </YAxis>
-          <ZAxis dataKey="investment" range={[10, 20]} name="score" unit="km" />
+          <ZAxis dataKey="investment" range={[10, 20]} name="investment" unit={'ETH'} />
+          {/* <Tooltip payload={[{ name: '05-01', value: 12, unit: 'kg' }]} /> */}
+          {/* <Tooltip content={<CustomTooltip payload={payload} />} /> */}
+          <Tooltip content={<CustomTooltip payload={trades} />} />
           {/*<Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Legend /> */}
           <ReferenceLine y="0" stroke="green" label="Break Even" strokeDasharray="3 3" alwaysShow={true} />
