ff377080f35ff13dbc092c5472aa89a0a97383db
Darshan <darshan.raju99@gmail.com>
2022-06-09 20:55:33 +1000

    handle can only have alphanumeric and underscore

--- pages/api/profile.tsx
+++ pages/api/profile.tsx
@@ -2,64 +2,81 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 import { getSession } from 'next-auth/react'
 
+const regex = /^[a-zA-Z0-9_]*$/
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const session = await getSession({ req })
+   const session = await getSession({ req })
 
-    if (!session || !session.userID || !session.user?.email) {
-        res.status(500).json({ message: 'Please log in first' })
-        return
-    }
+   if (!session || !session.userID || !session.user?.email) {
+      res.status(500).json({ message: 'Please log in first' })
+      return
+   }
 
-    if (req.method === 'POST') {
-        // adding email so the database constraint is satisfied
-        const authUserEmail: string = session.user.email
+   if (req.method === 'POST') {
+      // adding email so the database constraint is satisfied
+      const authUserEmail: string = session.user.email
 
-        let payloadData = {
-            ...JSON.parse(req.body),
-            email: authUserEmail, // attach the curent user's email
-        }
+      let payloadData = {
+         ...JSON.parse(req.body),
+         email: authUserEmail, // attach the curent user's email
+      }
 
-        const existingProfileWithEmail = await prisma.user_profile.findUnique({
+      const existingProfileWithEmail = await prisma.user_profile.findUnique({
+         where: {
+            email: authUserEmail,
+         },
+      })
+
+      // clean up the payloadData handle
+      payloadData = {
+         ...payloadData,
+         handle: payloadData.handle.replace(/\s/g, ''),
+      }
+
+      if (!payloadData.handle.match(regex)) {
+         res.status(500).json({
+            message: 'Handle can only contain a-z A-Z 0-9 or _',
+         })
+         return
+      }
+
+      //Check if handle is already in use by other user
+      const existingProfileWithHandle = await prisma.user_profile.findUnique({
+         where: {
+            handle: payloadData.handle.toLowerCase(),
+         },
+      })
+
+      if (
+         existingProfileWithHandle &&
+         existingProfileWithHandle.email.toLowerCase() !==
+            payloadData.email.toLowerCase()
+      ) {
+         res.status(500).json({
+            message:
+               'Profile handle has been taken. Please choose a different one.',
+         })
+         return
+      }
+
+      //Create new profile if email is new
+      if (!existingProfileWithEmail) {
+         // create a profile
+         await prisma.user_profile.create({
+            data: payloadData,
+         })
+         res.status(200).json({ message: 'Profile successfully created!' })
+      } else {
+         await prisma.user_profile.update({
             where: {
-                email: authUserEmail,
+               email: session.user.email,
             },
-        })
-
-        // clean up the payloadData handle
-        payloadData = { ...payloadData, handle: payloadData.handle.toLowerCase().replace(/\s/g, '') }
-
-        //Check if handle is already in use by other user
-        const existingProfileWithHandle = await prisma.user_profile.findUnique({
-            where: {
-                handle: payloadData.handle,
-            },
-        })
-
-        if (existingProfileWithHandle && existingProfileWithHandle.email.toLowerCase() !== payloadData.email) {
-            res.status(500).json({ message: 'Profile handle has been taken. Please choose a different one.' })
-            return
-        }
-
-        //Create new profile if email is new
-        if (!existingProfileWithEmail) {
-            // create a profile
-            await prisma.user_profile.create({
-                data: payloadData,
-            })
-            res.status(200).json({ message: 'Profile successfully created!' })
-
-        } else {
-
-            await prisma.user_profile.update({
-                where: {
-                    email: session.user.email,
-                },
-                data: payloadData,
-            })
-            res.status(200).json({ message: 'Profile successfully updated!' })
-        }
-    } else {
-        // Handle any other HTTP method
-        res.status(200).json({ message: 'Nothing happened.' })
-    }
+            data: payloadData,
+         })
+         res.status(200).json({ message: 'Profile successfully updated!' })
+      }
+   } else {
+      // Handle any other HTTP method
+      res.status(200).json({ message: 'Nothing happened.' })
+   }
 }
