db44ee5510ef2e54844112dfe81127fb1abe35f5
Junyu Yang <prowessyang@gmail.com>
2022-06-10 16:43:10 -0700

    removed google sign on

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+// import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 // import { PrismaClient } from '@prisma/client'
@@ -9,10 +9,10 @@ import prisma from '../../../lib/prisma'
 export default NextAuth({
    adapter: PrismaAdapter(prisma),
    providers: [
-      GoogleProvider({
-         clientId: process.env.GOOGLE_CLIENT_ID!,
-         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      }),
+      // GoogleProvider({
+      //    clientId: process.env.GOOGLE_CLIENT_ID!,
+      //    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      // }),
       EmailProvider({
          server: process.env.EMAIL_SERVER,
          from: process.env.EMAIL_FROM
