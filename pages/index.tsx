d5328a6627ab1e4ca930809de09ef1684fdf1d1c
Darshan <darshan.raju99@gmail.com>
2022-07-02 16:12:29 +1000

    Remove extra imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import { Session } from 'next-auth'
-import UnauthenticatedUser from '../components/UnauthenticatedUser'
 import { useSession } from 'next-auth/react'
 import Router from 'next/router'
 import { useEffect } from 'react'
-import Image from 'next/image'
-import { Box, Center } from '@chakra-ui/react'
 import Spinner from '../components/Spinner'
 
 export interface ESession extends Session {
